7371c00935d5929b05b52e686afdc22b760ee587
Nathan Gomes <nahtann@outlook.com>
2021-03-23 15:34:46 -0300

    delete category updated

--- src/controllers/CategoriesController.ts
+++ src/controllers/CategoriesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { getConnection, getRepository } from "typeorm";
 import Category from "../models/Category";
+import UpdateProductController from "./UpdateProductController";
 
 export default {
     // Return all categories from database
@@ -31,16 +32,25 @@ export default {
     },
 
     async deleteCategory(req: Request, res: Response) {
-        const nameId = req.params;
+        const { id } = req.params;
 
         const categoriesRepository = getRepository(Category);
 
         // if a category matching the [id] was not found, return a new error
-        const category = await categoriesRepository.findOneOrFail(nameId);
+        const category = await categoriesRepository.findOneOrFail(id);
+
+        // Removes the category entity from products
+        await UpdateProductController.removeCategoryEntity(Number(id))
 
         // if it was found, remove from the database and return a OK(200) status
         await categoriesRepository.remove(category);
-        
+
         return res.status(200).json(category);
+    },
+
+    async findCategory(id: number) {
+        const getCategoryRepository = getRepository(Category);
+
+        return await getCategoryRepository.findOneOrFail({ id });
     }
 }
